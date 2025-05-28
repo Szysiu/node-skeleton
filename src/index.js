f7caf274634998ad4080eda1afb34e2aab24e37a
Szysiu <lukasz.szysiak75@e-msi.pl>
2025-05-28 13:24:19 +0200

    project skeleton

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  console.log(`📥 Request received: ${req.method} ${req.url}`);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from container!\n');
+});
+
+server.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
